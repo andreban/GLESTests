f16ba7490ea02ea9fd97afea00fea43bda08cbe0
Andre Bandarra <andreban@gmail.com>
2011-09-29 17:28:05 -0300

    testing shaders inside renderer

--- src/com/mobplug/android/glestests/cubelightingtexture/CubeLightingTextureRenderer.java
+++ src/com/mobplug/android/glestests/cubelightingtexture/CubeLightingTextureRenderer.java
@@ -14,11 +14,47 @@ import com.mobplug.android.glestests.glutils.GLBatch;
 import com.mobplug.android.glestests.glutils.GLBatchFactory;
 import com.mobplug.android.glestests.glutils.GLFrustrum;
 import com.mobplug.android.glestests.glutils.GLShader;
-import com.mobplug.android.glestests.glutils.GLShaderFactory;
 import com.mobplug.android.glestests.glutils.GLTexture;
 import com.mobplug.android.glestests.glutils.MatrixStack;
 
 public class CubeLightingTextureRenderer extends AndroidGameRenderer3D<CubeLightingTextureGame> {
+	   private static final String VERTEX_SHADER =
+           "uniform mat4 mvMatrix;"
+           + "uniform mat4 pMatrix;"
+           + "uniform vec3 vLightPos;"
+           + "uniform vec4 inColor;"
+           + "attribute vec4 inVertex;"
+           + "attribute vec3 inNormal;"
+           + "varying vec4 vFragColor;"
+           + "attribute vec2 inTexCoord;"
+           + "varying vec2 vTex;"
+           + "void main(void) { "
+           + " mat3 mNormalMatrix;"
+           + " mNormalMatrix[0] = normalize(mvMatrix[0].xyz);"
+           + " mNormalMatrix[1] = normalize(mvMatrix[1].xyz);"
+           + " mNormalMatrix[2] = normalize(mvMatrix[2].xyz);"
+           + " vec3 vNorm = normalize(mNormalMatrix * inNormal);"
+//           + " vec4 ecPosition;"
+//           + " vec3 ecPosition3;"
+//           + " ecPosition = mvMatrix * inVertex;"
+//           + " ecPosition3 = ecPosition.xyz /ecPosition.w;"
+//           + " vec3 vLightDir = normalize(vLightPos - ecPosition3);"
+//           + " float fDot = max(0.0, dot(vNorm, vLightDir)); "
+//           + " vFragColor.rgb = inColor.rgb * fDot;"
+           + " vFragColor.a = inColor.a;"
+           + " vTex = inTexCoord;"
+           + " mat4 mvpMatrix;"
+           + " mvpMatrix = pMatrix * mvMatrix;"
+           + " gl_Position = mvpMatrix * inVertex; "
+           + "}";
+   private static final String FRAGMENT_SHADER =
+   	   "precision mediump float;"+	   
+//           "varying vec4 vFragColor;"
+            "varying vec2 vTex;"
+           + "uniform sampler2D textureUnit0;"
+           + "void main(void) { "
+           + " gl_FragColor = texture2D(textureUnit0, vTex);"
+           + "}";	
     private Context context;
 
     private GLShader shader;
@@ -69,7 +105,7 @@ public class CubeLightingTextureRenderer extends AndroidGameRenderer3D<CubeLight
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
 		GLES20.glClearColor(0.0f,0.0f,0.0f,0.0f);
 		GLES20.glEnable(GLES20.GL_DEPTH_TEST);		
-		shader = GLShaderFactory.getTexturePointLightDiffuseShader();
+		shader = new GLShader(VERTEX_SHADER, FRAGMENT_SHADER);
 		viewFrustrum = new GLFrustrum();
 		modelViewStack = new MatrixStack();   
 		texture0 = new GLTexture(context, R.raw.robot);				
